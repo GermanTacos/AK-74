12ed7449adfca602a80a7d3cb531e574a50fa036
IKillerwolfI <killerwolf.no1@gmail.com>
2019-12-19 23:07:59 +0100

    Sight stuff

--- hooks/weaponfactorytweakdata.lua
+++ hooks/weaponfactorytweakdata.lua
@@ -5,4 +5,44 @@ Hooks:PostHook( WeaponFactoryTweakData, "init", "AK74Init", function(self)
 -- GERMANTACOS, KILLERWOLF --
 --=========================--
 	
+--=========================--
+--          ARRAYS         --
+--=========================--
+
+local vanilla_ak545sight = {
+	"wpn_fps_upg_o_specter",
+	"wpn_fps_upg_o_aimpoint",
+	"wpn_fps_upg_o_aimpoint_2",
+	"wpn_fps_upg_o_docter",
+	"wpn_fps_upg_o_eotech",
+	"wpn_fps_upg_o_t1micro",
+	"wpn_fps_upg_o_cmore",
+	"wpn_fps_upg_o_acog",
+	"wpn_fps_upg_o_cs",
+	"wpn_fps_upg_o_eotech_xps",
+	"wpn_fps_upg_o_reflex",
+	"wpn_fps_upg_o_rx01",
+	"wpn_fps_upg_o_rx30",
+	"wpn_fps_upg_o_spot"
+}
+--=========================--
+--        	ADDS           --
+--=========================--
+--- Gun ---
+for id, o_id in pairs(vanilla_ak545sight) do
+	self.wpn_fps_ass_ak_stamp_545.adds[o_id] = {"wpn_fps_ass_ak_stamp_762_om_tula"}
+end
+--=========================--
+--          STANCE         --
+--=========================--
+
+--- Vanilla sights ---
+for id, o_id in pairs(vanilla_ak545sight) do
+	if self.parts[o_id].stance_mod.wpn_fps_ass_flint then
+		self.parts[o_id].stance_mod.wpn_fps_ass_ak_stamp_545 = deep_clone(self.parts[o_id].stance_mod.wpn_fps_ass_flint)
+	else
+		log("[AK_Pack] [ERROR] WHO THE FUCK MESSED WITH SIGHT STANCES THIS TIME!? " .. o_id)
+	end
+end
+
 end )
--- hooks/weapontweakdata.lua
+++ hooks/weapontweakdata.lua
@@ -1,5 +1,17 @@
 Hooks:PostHook( WeaponTweakData, "init", "AK74Init", function(self)
 
+--- TBC ---
+if self.new_m4.reload_timed_stance_mod then
+	self.ak_stamp_545.reload_timed_stance_mod = {
+		empty = {
+			hip = {
+				{t = 1.2, translation = Vector3(-25, 18, -10), rotation = Rotation(-50, 0, -40), speed = 0.5}, -- turn gun over to check that bolt has released
+				{t = 0.0, translation = Vector3(0, 0, 0), rotation = Rotation(0, 0, 0), speed = 0.5} -- return to default position
+			}
+		}
+	}
+end
+
 if ( self.ak_stamp_545 ) then
 	
 	self:SetupAttachmentPoint("ak_stamp_545", {
@@ -16,6 +28,12 @@ if ( self.ak_stamp_545 ) then
         rotation = Rotation( 0, 0, 0 ) 
 	})
 	
+	self:SetupAttachmentPoint("ak_stamp_545", {
+		name = "a_o", 
+        base_a_obj = "a_o", 
+        position = Vector3( 0, -6, -0.5 ), 
+        rotation = Rotation( 0, 0, 0 ) 
+	})
 end
 
 end)
